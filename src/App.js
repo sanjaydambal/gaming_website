a907b4d48a1cee38ce26437c39f3dc1af87bebf8
sanjaydambal <sanjaydambal@gmail.com>
2024-03-16 12:10:25 +0530

    created gh pages and updated the App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import { createBrowserRouter, Routes, Route,RouterProvider
  } from 'react-router-dom';
-
+ import { AuthProvider } from './Auth'
 import Home from './Home';
 import Login from './Login'; 
 import Register from './Register';
-<RouterProvider router={router}>
-  <App /> 
-</RouterProvider>
+
+import GamingDashboard from './GamingDashboard';
+import News from './News';
+import SpecificNews from './SpecificNews';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    
     children: [
       {
-        path: "/login",
+        path: "login",
         element: <Login />,
       },
-        {
-        path: "/register",
+      {
+        path: "register",
         element: <Register />,
       },
+      {
+        path: "dashboard",
+        element: <GamingDashboard />,
+      },
+      {
+        path: "news",
+        element: <News />,
+      },
+      // Separate route for specific news item
+      {
+        path: "news/:id",
+        element: <SpecificNews />,
+      }
     ],
   },
-])
+]);
 
 function App() {
   
   return (
-    <Home/>
+    <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,53 +1,19 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
-import Home from './Home';
-import Login from './Login'; 
-import Register from './Register';
-import GamingDashboard from './GamingDashboard';
-import News from './News';
-import { AuthProvider } from './Auth'
-import SpecificNews from './SpecificNews';
+
+import App from './App';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-      {
-        path: "dashboard",
-        element: <GamingDashboard />,
-      },
-      {
-        path: "news",
-        element: <News />,
-      },
-      // Separate route for specific news item
-      {
-        path: "news/:id",
-        element: <SpecificNews />,
-      }
-    ],
-  },
-]);
+
 
 root.render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+  <App/>
   </StrictMode>
 );
 
